5b16777234d6ba0af96fd019bf8b604d4763e4ad
Thad Duval <witherberry@thads-mbp.attlocal.net>
2017-01-03 14:08:06 -0500

    custom tab bar icons and size done

---
+++ Test/TabBar.swift
@@ -0,0 +1,21 @@
+//
+//  TabBarVC.swift
+//  Test
+//
+//  Created by Thad Duval on 1/3/17.
+//  Copyright © 2017 Thad Duval. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class TabBar: UITabBar {
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        var sizeThatFits = super.sizeThatFits(size)
+        sizeThatFits.height = 80
+        
+        return sizeThatFits
+    }
+    
+}
